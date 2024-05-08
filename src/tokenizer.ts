bb0e80be080745cb3883f7d2ecc230a6d01a0bc9
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2024-05-08 15:46:35 +0530

    Initial commit

---
+++ src/tokenizer.ts
@@ -0,0 +1,12 @@
+import {get_encoding as getEncoding} from '@dqbd/tiktoken'
+
+const tokenizer = getEncoding('cl100k_base')
+
+export function encode(input: string): Uint32Array {
+  return tokenizer.encode(input)
+}
+
+export function getTokenCount(input: string): number {
+  input = input.replace(/<\|endoftext\|>/g, '')
+  return encode(input).length
+}
